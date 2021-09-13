24a18bd36405ccbf9cab68a9eaa608beef440e7c
watanabe@tiffa <watanabe.everystudio@gmail.com>
2021-09-13 21:07:53 +0900

    プレイヤーの移動処理

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,21 @@ using anogamelib;
 
 public class PlayerController : StateMachineBase<PlayerController>
 {
+	private float m_fMovementSpeed = 3.0f;
+	private Animator m_animator;
 	private void Start()
 	{
+		m_animator = GetComponent<Animator>();
 		SetState(new PlayerController.Idle(this));
 	}
+
+	private void SetDir(Vector2 _dir)
+	{
+		m_animator.SetFloat("dir_x", _dir.normalized.x);
+		m_animator.SetFloat("dir_y", _dir.normalized.y);
+	}
+
+
 
 	private class Idle : StateBase<PlayerController>
 	{
@@ -27,7 +38,12 @@ public class PlayerController : StateMachineBase<PlayerController>
 		{
 			base.OnUpdateState();
 
-			machine.transform.Translate(m_gameInput.Player.Move.ReadValue<Vector2>()*Time.deltaTime);
+			Vector2 vec2Movement = m_gameInput.Player.Move.ReadValue<Vector2>();
+			machine.transform.Translate(vec2Movement * machine.m_fMovementSpeed * Time.deltaTime);
+			if (vec2Movement != Vector2.zero)
+			{
+				machine.SetDir(vec2Movement);
+			}
 		}
 
 	}
